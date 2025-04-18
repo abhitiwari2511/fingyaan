6b3929787be60b3101abf0db71acf5df1fe8c1ea
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2025-04-19 04:08:13 +0530

    cors err fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,28 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true
-}));
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors());
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 // routes import
-import userRouter from "./routes/user"
+import userRouter from "./routes/user";
 
 // routes declaration
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
 
-export default app
+export default app;
